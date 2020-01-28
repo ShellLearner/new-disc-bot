af8e337e54dba112d870c08f5a4c755d67036716
ShellLearner <logando@gmail.com>
2020-01-28 16:14:32 -0500

    added starting bot code

--- bot.js
+++ bot.js
@@ -1,9 +1,23 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 const auth = require('./auth.json');
+var prefix = "!"
+
 
 client.on('ready', () => {
 	console.log("AYOOO ok we're logged in as ${client.user.tag}")
+});
+
+client.on('message', message => {
+	if (message.author.bot)
+		return;
+	else if (message.content.startsWith(prefix))
+		if (message.content.startsWith(prefix + 'ping')) { 
+			message.channel.send("Pong! Hello!");
+		} else if (message.content.startsWith(prefix + "prefix")
+			message.content.replace(prefix, "");
+			message.content.replace(" ", "");
+			prefix = message.content
 });
 
 client.login(auth.token);
