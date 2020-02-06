43351d02baf43a11ab60ac8b6e5ff9827def11a1
logando <logando@icloud.com>
2020-02-06 12:54:09 -0500

    Cut code part, stripped to bare bones: will add flesh later

--- bot.js
+++ bot.js
@@ -5,21 +5,9 @@ var prefix = "!"
 
 
 client.on('ready', () => {
-	console.log("AYOOO ok we're logged in as ${client.user.tag}")
+	console.log(`AYOOO ok logged in as ${client.user.tag}`)
 });
 
-client.on('message', message => {
-	if (message.author.bot) {
-		return;
-	} else if (message.content.startsWith(prefix)) {
-		message.content.replace(prefix, "")
-		if (message.content.startsWith('ping')) { 
-			message.channel.send("Pong! Hello!");
-		} else if (message.content.startsWith("prefix")
-			message.content.replace(" ", "");
-			prefix = message.content
-			message.channel.send("Your prefix is " + prefix) 
-	}
-});
+
 
 client.login(auth.token);
