559e8195a8a1ac4ba64eefb9c690379e7372fc98
logando <logando@icloud.com>
2020-02-07 12:23:26 -0500

    added beginning code

--- bot.js
+++ bot.js
@@ -8,6 +8,13 @@ client.on('ready', () => {
 	console.log(`AYOOO ok logged in as ${client.user.tag}`)
 });
 
-
+client.on("message", message => {
+	if (message.content.startsWith(prefix)) {
+		message.content.replace(prefix + " ", "");
+		if (message.content.startsWith(ping)) {
+			message.channel.send("Pong!");
+		}
+	}
+});
 
 client.login(auth.token);
