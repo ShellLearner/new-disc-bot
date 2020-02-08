aadb0c13239d434c2fe6abf37665229e42c2a2a8
logando <logando@icloud.com>
2020-02-08 16:36:25 -0500

    added a comments file

--- bot.js
+++ bot.js
@@ -1,8 +1,8 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 const auth = require('../auth.json');
+const fs = require("fs");
 var prefix = "!"
-
 
 client.on('ready', () => {
 	console.log(`AYOOO ok logged in as ${client.user.tag}`)
@@ -13,6 +13,13 @@ client.on("message", message => {
 		message.content.replace(prefix + " ", "");
 		if (message.content.startsWith(ping)) {
 			message.channel.send("Pong!");
+		} else if (message.content.startsWith("comment ")){
+			message.content.replace("comment ", "");
+			console.log(message.content);
+			message.channel.send("Comment logged. Thank you for your feedback!");
+			fs.writeFile(comments.txt, message.content, err => {
+				if (err) throw err
+			});
 		}
 	}
 });
