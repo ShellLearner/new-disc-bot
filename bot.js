56d8e5f57f4bed946cb8a9aef710b376da4f74fb
logando <logando@icloud.com>
2020-02-10 07:32:23 -0500

    did lots of things

--- bot.js
+++ bot.js
@@ -2,26 +2,34 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 const auth = require('../auth.json');
 const fs = require("fs");
-var prefix = "!"
+var prefix = "??"
 
 client.on('ready', () => {
 	console.log(`AYOOO ok logged in as ${client.user.tag}`)
 });
 
-client.on("message", message => {
-	if (message.content.startsWith(prefix)) {
-		message.content.replace(prefix + " ", "");
-		if (message.content.startsWith(ping)) {
+client.on('message', message => {
+
+	if (message.author.bot){
+		return;
+
+	} else if (message.content.startsWith(prefix)) {
+		console.log("A command");
+		var command = message.content.replace(prefix, "")
+
+		if (command.startsWith("ping")) {
 			message.channel.send("Pong!");
-		} else if (message.content.startsWith("comment ")){
-			message.content.replace("comment ", "");
-			console.log(message.content);
-			message.channel.send("Comment logged. Thank you for your feedback!");
-			fs.writeFile(comments.txt, message.content, err => {
-				if (err) throw err
-			});
+			console.log("Ping recieved");
+		} else if (command.startsWith("comment")) {
+			var comment = command.replace("comment ", "")
+				
+			fs.writeFile(comments.txt, comment, err => {
+				if (err) {throw err}
+			}
+			
 		}
 	}
 });
+
 
 client.login(auth.token);
